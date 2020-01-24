009bb12e2a2d396c7e229eef7979b1231a123510
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-24 19:56:12 +0000

    model added

--- multiplayer/multiplayer.js
+++ multiplayer/multiplayer.js
@@ -28,6 +28,7 @@ function startTimer(duration, display) {
     display.textContent = seconds + "s";
 
     if (--timer < 0) {
+      changeTurn()
       timer = duration;
     }
 
@@ -40,7 +41,7 @@ function startTimer(duration, display) {
 }
 
 window.onload = () => {
-  playTime = 24;
+  playTime = 14;
   display = document.querySelector('#timer');
   startTimer(playTime, display);
 };
@@ -87,8 +88,8 @@ const divTable = document.getElementById("divTable")
 divTable.innerHTML = table
 console.log(document.getElementById("divTable"));
 }
-saveData()
-
+/* saveData() */
+/* 
 for (const name of names) {
   if (name.player1) {
     document.getElementById("player1").innerHTML = name.player1
@@ -96,4 +97,62 @@ for (const name of names) {
 if (name.player2) {
   document.getElementById("player2").innerHTML = name.player2
 }
+} */
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+window.onload = function() {
+  modal.style.display = "block";
+}
+const obj1
+const obj2
+let names = []
+document.querySelector("#btnStart").addEventListener("click", setSessionStorage);
+function setSessionStorage(event) {
+    const txtPlayer1 = document.getElementById("player1").value
+        const txtPlayer2 = document.getElementById("player2").value
+        if (txtPlayer1 != "" && txtPlayer2 != "") {
+              obj1 = {player:txtPlayer1, points: 0}
+              obj2 = {player:txtPlayer2, points: 0} 
+              names.push(obj1)
+              names.push(obj2)
+              sessionStorage.setItem("nome", JSON.stringify(names))
+        }
+    event.preventDefault()
+    console.log(document.getElementsByTagName("form")[0].checkValidity());
+    if (document.getElementsByTagName("form")[0].checkValidity()) {
+      modal.style.display = "none";
+    }  
+saveLocalData()
+}
+function saveLocalData() {
+if (localStorage.getItem("playersNames") == null) {
+    if (JSON.parse(sessionStorage.getItem("nome")) != null) {
+      localStorage.setItem("playersNames", JSON.stringify(JSON.parse(sessionStorage.getItem("nome"))))
+    }
+} else 
+
+if (localStorage.getItem("playersNames") != null) {
+    names = []
+    for (let i = 0; i < JSON.parse(localStorage.getItem("playersNames")).length; i++) {
+        names.push(JSON.parse(localStorage.getItem("playersNames"))[i])
+        if (obj1.player != JSON.parse(localStorage.getItem("playersNames"))[i].player) {
+          names.push(obj1)
+        }
+        if (obj2.player != JSON.parse(localStorage.getItem("playersNames"))[i].player) {
+          names.push(obj2)
+        }
+    } 
+    console.log(names);
+localStorage.setItem("playersNames", JSON.stringify(names))
+}
+}
+
+function refreshPoints() {
+  for (let i = 0; i < JSON.parse(localStorage.getItem("playersNames")).length; i++) {
+if (obj1.player == JSON.parse(localStorage.getItem("playersNames"))[i].player) {
+  if (gameWon == true) {
+  
+  }
+}}
 }
